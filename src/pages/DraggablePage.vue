7da8954ea33256223a9a416d755bb81a25db8b94
mateuszmistecki <mateusz.mistecki@gmail.com>
2023-09-05 23:37:53 +0200

    Add DraggablePage.vue

---
+++ src/pages/DraggablePage.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+  <q-page padding>
+    <q-btn class="q-ma-sm">Source btn</q-btn>
+    <q-btn class="q-ma-sm">Destination btn</q-btn>
+  </q-page>
+</template>
